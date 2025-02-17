c8a2acf7aca891e91382fdef35a0edb4b4ef397b
Sijmen de Bruijn <15785203+sdebruijn@users.noreply.github.com>
2025-02-17 13:21:07 +1100

    Add aws profile menu

--- zsh/.config/zsh/fzf.zsh
+++ zsh/.config/zsh/fzf.zsh
@@ -45,3 +45,21 @@ _fzf_comprun() {
     *)            fzf "$@" ;;
   esac
 }
+
+
+choose_aws_profile() {
+  # Extract profile names from the AWS config file
+  selected_profile=$(
+    sed -n "s/\[profile \(.*\)\]/\1/gp" ~/.aws/config \
+    | fzf \
+    --height=20% \
+    --min-height=12 \
+    --margin=5%,2%,2%,5% \
+    --border=rounded \
+    --info=inline \
+    --header="Choose AWS profile" \
+  )
+  export AWS_PROFILE=$selected_profile
+  echo "Set AWS profile to '$selected_profile'"
+}
+alias awsprofile='choose_aws_profile'
