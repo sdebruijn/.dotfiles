646f56f78e25fb86f7640e15786667396f9ce842
Sijmen de Bruijn <15785203+sdebruijn@users.noreply.github.com>
2025-02-17 13:23:07 +1100

    Add tmux config in neovim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -96,6 +96,25 @@ require('lazy').setup({
   },
 
   {
+      "christoomey/vim-tmux-navigator",
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+    "TmuxNavigatePrevious",
+    "TmuxNavigatorProcessList",
+  },
+  keys = {
+    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+  },
+  },
+
+  {
     -- Autocompletion
     'hrsh7th/nvim-cmp',
     dependencies = {
