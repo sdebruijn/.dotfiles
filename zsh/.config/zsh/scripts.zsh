bd8a6e11a8392d518d262cd1ecbb6638c30c916e
Sijmen de Bruijn <15785203+sdebruijn@users.noreply.github.com>
2025-02-18 16:15:45 +1100

    Add script for eos database dump

--- zsh/.config/zsh/scripts.zsh
+++ zsh/.config/zsh/scripts.zsh
@@ -1,3 +1,23 @@
 #!/usr/bin/env zsh
 
 # Place for custom scripts
+function dumpeos() {
+  echo 'Start database dump eos';
+
+  pg_dump \
+    --no-owner \
+    --no-acl \
+    --column-inserts \
+    --data-only \
+    --exclude-table='*_id_seq' \
+    --exclude-table-data='*_id_seq' \
+    --exclude-table=migrations \
+    --exclude-table=api_clients \
+    --exclude-table=failed_jobs \
+    --exclude-table=ledgers \
+    --exclude-table=msg_directions \
+    "service=eos" \
+     > eos-db-$(date +%Y-%m-%d-%H%M).sql  
+
+    echo "Saved dump to eos-db-$(date +%Y-%m-%d-%H%M).sql";
+}
