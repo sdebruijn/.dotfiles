74bd5b80779a3cc1f31a4be03f37499ced1e14f9
Sijmen de Bruijn <15785203+sdebruijn@users.noreply.github.com>
2025-02-25 14:22:38 +1100

    Add fzf script for database dump

--- zsh/.config/zsh/scripts_fzf.zsh
+++ zsh/.config/zsh/scripts_fzf.zsh
@@ -17,3 +17,47 @@ choose_aws_profile() {
   echo "Set AWS profile to '$selected_profile'"
 }
 alias awsprofile='choose_aws_profile'
+
+
+function dbdump() {
+  PG_SERVICE_CONF="$HOME/.pg_service.conf"
+
+  if [[ ! -f "$PG_SERVICE_CONF" ]]; then
+    echo "File $PG_SERVICE_CONF not found!" >&2
+    exit 1
+  fi
+
+  # Extract service names (lines starting with [)
+  SERVICE=$(grep -E '^\[.*\]' "$PG_SERVICE_CONF" | tr -d '[]' |
+    fzf \
+    --height=20% \
+    --min-height=12 \
+    --margin=5%,2%,2%,5% \
+    --border=rounded \
+    --info=inline \
+    --header="Choose database" \
+  )
+  if [[ -z "$SERVICE" ]]; then
+    echo "No service selected." >&2
+    exit 1
+  fi
+
+  echo "Start database dump of $SERVICE";
+
+  pg_dump \
+    --no-owner \
+    --no-acl \
+    --column-inserts \
+    --data-only \
+    --exclude-table='*_id_seq' \
+    --exclude-table-data='*_id_seq' \
+    --exclude-table=migrations \
+    --exclude-table=api_clients \
+    --exclude-table=failed_jobs \
+    --exclude-table=ledgers \
+    --exclude-table=msg_directions \
+    "service=$SERVICE" \
+     > $SERVICE-db-$(date +%Y-%m-%d-%H%M).sql  
+
+    echo "Saved dump to $SERVICE-db-$(date +%Y-%m-%d-%H%M).sql";
+}
